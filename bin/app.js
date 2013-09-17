4d45f48464ed77567c9c95d915f16e116b6fcdf7
Jen Andre <jandre@gmail.com>
2013-09-17 19:14:20 -0400

    auth

--- bin/app.js
+++ bin/app.js
@@ -7,8 +7,11 @@ var express = require('express')
   , routes = require('../routes')
   , user = require('../routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , config = require('../config')
+  , server = require('../lib/server');
 
+var passport = require('passport');
 var app = express();
 
 // all environments
@@ -19,6 +22,10 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use( express.cookieParser() );
+app.use(express.session({ secret: 'afsjljX)FJfejejneX' }));  // XXX: not hardcoded
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,7 +37,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-app.get("/fetch/ip", function(req, res) {
+app.get('/fetch/ip', function(req, res) {
   var ip_address = null;
   if(req.headers['x-forwarded-for']){
     ip_address = req.headers['x-forwarded-for'];
@@ -43,7 +50,11 @@ app.get("/fetch/ip", function(req, res) {
   });
 });
 
+app.post('/login', server.auth.login);
+app.get('/login', server.auth.login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+server.auth.setup();
