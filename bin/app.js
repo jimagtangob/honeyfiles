047e9930267eeb1f08cdb8f6c18c549b761e2ce2
Dustin Willis Webber <dustin.webber@gmail.com>
2013-09-17 18:20:06 -0400

    update

--- bin/app.js
+++ bin/app.js
@@ -27,7 +27,8 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public/'));
 
 // development only
 if ('development' == app.get('env')) {
